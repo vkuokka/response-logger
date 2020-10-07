cae927bf1ff214272de134491167c43df76e812d
Vesa Kuokka <vkuokka@student.hive.fi>
2020-10-07 15:13:12 +0300

    Small changes in logger file.

--- app/logger.py
+++ app/logger.py
@@ -8,13 +8,15 @@ class Logger:
 	def log(self, response, url_dict):
 		try:
 			file = open(self.logfile, 'a+')
-			file.write(f"Sent:			{'{0:%Y-%m-%d %H:%M:%S}'.format(datetime.now())}\n")
-			file.write(f"URL:			{str(response.url)}\n")
-			file.write(f"Response:		{str(response.elapsed.total_seconds())}\n")
-			file.write(f"Code:			{('Correct' if url_dict['code'] == response.status_code else 'Incorrect')}\n")
-			file.write(f"Content-Type:	{('Correct' if url_dict['content-type'] in response.headers['content-type'] else 'Incorrect')}\n")
-			file.write(f"Content:		{('Correct' if url_dict['content'] in response.text else 'Incorrect')}\n")
-			file.write('\n')
+			file.write(
+				f"Sent:			{'{0:%Y-%m-%d %H:%M:%S}'.format(datetime.now())}\n" +
+				f"URL:			{str(response.url)}\n" +
+				f"Response:		{str(response.elapsed.total_seconds())}\n" +
+				f"Code:			{('PASS' if url_dict['code'] == response.status_code else 'FAIL')}\n" +
+				f"Content-Type:	{('PASS' if url_dict['content-type'] in response.headers['content-type'] else 'FAIL')}\n" +
+				f"Content:		{('PASS' if url_dict['content'] in response.text else 'FAIL')}\n" +
+				'\n'
+			)
 			file.close()
 		except IOError:
 			raise SystemExit('Problems with logger.')
@@ -22,10 +24,12 @@ class Logger:
 	def error(self, url, message):
 		try:
 			file = open(self.logfile, 'a+')
-			file.write(f"Sent:			{'{0:%Y-%m-%d %H:%M:%S}'.format(datetime.now())}\n")
-			file.write(f"URL:			{str(url)}\n")
-			file.write(f"Exception:		{message}\n")
-			file.write('\n')
+			file.write(
+				f"Sent:			{'{0:%Y-%m-%d %H:%M:%S}'.format(datetime.now())}\n" +
+				f"URL:			{str(url)}\n" +
+				f"Exception:		{message}\n" +
+				'\n'
+			)
 			file.close()
 		except IOError:
 			raise SystemExit('Problems with logger.')
